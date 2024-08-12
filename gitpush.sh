8149f1808c95987b58c5b99bd3ded8c5f641aacf
dltv19 <sultandaulatov@gmail.com>
2024-08-12 11:50:59 +0500

    Portfolio

---
+++ gitpush.sh
@@ -0,0 +1,15 @@
+# 1 пуш
+# git init
+# git add .
+# git commit -m "ConstructionCompany2"
+# git remote add origin git@github.com:daulyatov/Portfolio.git
+# git push origin master
+
+# след пуши 
+git add .
+git commit -m " 4 commit"
+git push origin master
+
+# при ошибках
+# git pull origin master --rebase
+# git push origin master
